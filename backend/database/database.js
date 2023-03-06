323ea03818fb7a156bee9f960c7ebb775ef9cb30
doomsday26 <105936321+doomsday26@users.noreply.github.com>
2023-03-06 22:24:21 +0530

    Delete database.js

--- backend/database/database.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize= new Sequelize('bookingappointment','root','harsh226748',{
-    dialect:'mysql',host:'localhost'
-})
-module.exports=sequelize;
